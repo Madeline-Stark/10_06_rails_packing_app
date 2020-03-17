8e8a90b4a152a42a1bfc7191dfb5cc68346ed716
github username <github email address>
2020-03-17 15:59:25 -0700

    Nested Forms ctd

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,27 @@
 class ListsController < ApplicationController
+
+    def index 
+        @lists = List.all
+    end 
+
+    def show
+        @list = List.find(params[:id])
+    end 
+
+    def new
+        @list = List.new
+        2.times { @list.items.build}
+    end 
+
+    def create 
+        @list = List.create(list_params)
+        redirect_to list_path(@list)
+    end 
+
+    private
+
+    def list_params
+        params.require(:list).permit(:name, items_attributes: [:name, :quantity])
+    end
+
 end
